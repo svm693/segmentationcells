03acd22adf2ca56a193dafdebfeb9cb25c1e008e
sav5134 <sav5134@BME-020200006.coeaccess.psu.edu>
2018-05-23 23:49:21 -0400

    reduced input size to 568 by 568 and network ran succesfully in 6 minutes.

--- Shen.m
+++ Shen.m
@@ -24,73 +24,124 @@
 %}
 
 
+clc, clearvars
 
 % Create ground truth
-dir={'F:\Box Sync\Bioengineering\Research\Dr. Wong Lab\Data\MATLAB\SCM-Sandbox\RCNN-Segm\RCNN-SCM-gTruth\Trial'};
-imDir=fullfile(dir,'images');
+dir={'F:\Box Sync\Bioengineering\Research\Dr. Wong Lab\Data\MATLAB\segmentationcells-Sandbox'};
+imDir=fullfile(dir,'Images');
 labelDir=fullfile(dir,'PixelLabelData');
-NewimDir=fullfile(dir,'Partitioned','Images');
-NewlabelDir=fullfile(dir,'Partitioned','PixelLabelData');
 
-cd(char(NewimDir))
-if exist('I1.jpg', 'file') == 2
+cd(char(imDir))
+if exist('I111.png', 'file') == 2
 
 else
-    I=imread(fullfile(char(dir),'RT4-182.tif'));
-    I=im2single(I);
+    cd(char(dir))
+    I{1}=imread('RT4-180.tif');
+    I{2}=imread('RT4-182.tif');
+    
+    [p,q,~]=size(I)
 
-    % divide images into 4 smaller regions
-    [a,b,~]=size(I);
-    I1=I(1:a/2,1:a/2); %top left
-    I2=I((a/2)+1:a,1:a/2); %bottom left
-    I3=I(1:a/2,a/2+1:a); %top right
-    I4=I(a/2+1:a,a/2+1:a); %bottom right
+    for k=1:q
+          
+        I{k}=im2single(I{k});  
 
-    imwrite(I1,'I1.jpg');
-    imwrite(I2,'I2.jpg');
-    imwrite(I3,'I3.jpg');
-    imwrite(I4,'I4.jpg');
+        % divide images into non-overlapping blocks of 568 by 568
+        n=568;
+        [a,b,~]=size(I{k});
+        B=cell(a/n,b/n);
+        cd(char(imDir))
+        
+        i=1;
+        ci=0;
+        for i = 1:n:a-(n-1)
+            ci = ci + 1;
+            j=1;
+            cj=0;
+            for j = 1:n:b-(n-1)
+                cj = cj + 1;
+                B{ci,cj} = I{k}(i:i+(n-1),j:j+(n-1));
+                name=sprintf('I%d%d%d.png',k,ci,cj);
+                imwrite(B{ci,cj},name)
+            end
+        end 
+    end
+end
+
+cd(char(labelDir))
+if exist('L111.png','file')==2
+
+else
+    cd(char(dir))
+    L{1}=imread('Label_180.png');
+    L{2}=imread('Label_182.png');
+    
+    [p,q,~]=size(L)
+
+    for k=1:q       
+        
+        L{k}=im2single(L{k});  
+
+        % divide images into non-overlapping blocks of 568 by 568
+        n=568;
+        [a,b,~]=size(L{k});
+        B=cell(a/n,b/n);
+        cd(char(labelDir))
+        
+        i=1;
+        ci=0;
+        for i = 1:n:a-(n-1)
+            ci = ci + 1;
+            j=1;
+            cj=0;
+            for j = 1:n:b-(n-1)
+                cj = cj + 1;
+                B{ci,cj} = L{k}(i:i+(n-1),j:j+(n-1));
+                name=sprintf('L%d%d%d.png',k,ci,cj);
+                imwrite(B{ci,cj},name)
+            end
+        end 
+    end
 end
 
 
-
-cd(char(NewlabelDir))
-if exist('L1.png','file')==2
-
-else
-    L=imread(fullfile(char(labelDir),'Label_223.png')); %verify with Edit>Colormap>Color data max: 3
-    L=im2single(L);
-    
-    L1=L(1:a/2,1:a/2); %top left
-    L2=L((a/2)+1:a,1:a/2); %bottom left
-    L3=L(1:a/2,a/2+1:a); %top right
-    L4=L(a/2+1:a,a/2+1:a); %bottom right
-    
-    imwrite(L1,'L1.png');
-    imwrite(L2,'L2.png');
-    imwrite(L3,'L3.png');
-    imwrite(L4,'L4.png');
-end
+%%
 
 cd(char(dir))
+% convert unit8 to single for MATLAB processing
+
 % re-create ground truth for image I and label L
-% load ground truth with labeled image 223 (rt4-182)
-load('F:\Box Sync\Bioengineering\Research\Dr. Wong Lab\Data\MATLAB\SCM-Sandbox\RCNN-Segm\RCNN-SCM-gTruth\Try-1image\gTruth-1.mat');
+load('F:\Box Sync\Bioengineering\Research\Dr. Wong Lab\Data\MATLAB\SCM-Sandbox\RCNN-Segm\RCNN-SCM-gTruth\Trial\gTruth2im.mat');
 labelDefs=gTruth.LabelDefinitions;
 classes=labelDefs.Name;
-pxds=pixelLabelDatastore(NewlabelDir,classes,labelDefs.PixelLabelID);
-imds=imageDatastore(NewimDir);
+pxds=pixelLabelDatastore(labelDir,classes,labelDefs.PixelLabelID);
+imds=imageDatastore(imDir);
 
-% %Confirm and display
-% Im = readimage(imds,2);
-% C = readimage(pxds,2);
-% L=uint8(C);
-% imshowpair(Im,L,'montage')
+%Confirm and display
+Im = readimage(imds,2);
+C = readimage(pxds,2);
+C2=uint8(C);
+imshowpair(Im,C2,'montage')
 
 % create and partition data
 allData=pixelLabelImageDatastore(imds,pxds);
-trainingData=partitionByIndex(allData,[1 3 4]);
-valData=partitionByIndex(allData,2);
+% partition data in 75/25... 96 images for training and 128-96=32 for
+% validation
+%---------------------
+tVector=randperm(128,96);
+vVector=zeros(1,32);
+j=1;
+for i=1:128
+    num=find(tVector == i);
+   if num > 0
+   
+   else
+      vVector(j)=i;
+      j=j+1;
+   end    
+end
+
+trainingData=partitionByIndex(allData,tVector);
+valData=partitionByIndex(allData,vVector);
 
 % To adjust class inbalance
 % view: https://www.mathworks.com/examples/matlab-computer-vision/mw/vision-ex41887170-semantic-segmentation-using-deep-learning?s_tid=examples_p1_MLT
@@ -139,11 +190,11 @@ BFsegNet = [
 analyzeNetwork(BFsegNet)
 
 
-setup training options
+% setup training options
 opts = trainingOptions('sgdm', ...
     'InitialLearnRate',1e-3, ...
     'MaxEpochs',100, ...
-    'MiniBatchSize',4, ...
+    'MiniBatchSize',2, ...
     'Plots','training-progress', ...
     'ExecutionEnvironment', 'GPU', ...
     'ValidationData',valData);
@@ -156,7 +207,9 @@ tBFseg=trainNetwork(trainingData,BFsegNet,opts);
 
 
 % test net through imds and pxds
-testI=readimage(imds,4);
+% view: https://www.mathworks.com/help/vision/ug/semantic-segmentation-examples.html#mw_9ca2a7be-c8c2-4bbf-b168-128261d1be7d
+
+testI=readimage(imds,7);
 testC=semanticseg(testI,tBFseg);
 B=labeloverlay(testI,testC);
 figure
